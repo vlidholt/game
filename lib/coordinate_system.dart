885c83f2bb2fb7fd7b7590293b5b7027c94b486d
Adam Barth <abarth@chromium.org>
2016-03-13 10:26:03 -0700

    [rename fixit] Update to use new names
    Update the game to use the new names from the rename fixit.

--- lib/coordinate_system.dart
+++ lib/coordinate_system.dart
@@ -1,6 +1,6 @@
 part of game;
 
-class CoordinateSystem extends OneChildRenderObjectWidget {
+class CoordinateSystem extends SingleChildRenderObjectWidget {
   CoordinateSystem({ Key key, this.systemSize, this.systemType: CoordinateSystemType.fixedWidth, Widget child })
     : super(key: key, child: child) {
     assert(systemSize != null);
@@ -9,9 +9,11 @@ class CoordinateSystem extends OneChildRenderObjectWidget {
   final Size systemSize;
   final CoordinateSystemType systemType;
 
-  RenderCoordinateSystem createRenderObject() => new RenderCoordinateSystem(systemSize: systemSize, systemType: systemType);
+  RenderCoordinateSystem createRenderObject(BuildContext context) {
+    return new RenderCoordinateSystem(systemSize: systemSize, systemType: systemType);
+  }
 
-  void updateRenderObject(RenderCoordinateSystem renderObject, CoordinateSystem oldWidget) {
+  void updateRenderObject(BuildContext context, RenderCoordinateSystem renderObject) {
     renderObject.systemSize = systemSize;
     renderObject.systemType = systemType;
   }
